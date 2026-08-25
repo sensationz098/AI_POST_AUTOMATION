2376e31cf2b9e20bbb5c30101dc16871e3d8226b
Soubhagya108 <soubhagyavashistha@gmail.com>
2026-08-25 16:13:58 +0530

    changed config cors and added vercel link specifically

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -90,11 +90,9 @@ class Settings(BaseSettings):
     # CORS
     BACKEND_CORS_ORIGINS: Union[List[str], str] = Field(
         default=[
-            # "http://localhost:3000",
-            # "http://127.0.0.1:3000",
-            # "https://ai-post-automation.vercel.app",
-            # "https://ai-post-automation-sensationz.vercel.app",
-            "*"
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://ai-post-automation-sensationz.vercel.app"
         ],
         env="BACKEND_CORS_ORIGINS"
     )
