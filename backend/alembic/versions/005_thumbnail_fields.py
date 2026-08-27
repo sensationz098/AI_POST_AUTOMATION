6ea555d2d1c6926f1ee823dfd921b1088d47635f
Soubhagya180 <soubhagyavashistha@gmail.com>
2026-08-27 17:17:57 +0530

    fix: shorten thumbnail migration revision

--- backend/alembic/versions/005_thumbnail_fields.py
+++ backend/alembic/versions/005_thumbnail_fields.py
@@ -1,6 +1,6 @@
 """Add nullable video thumbnail columns to posts table
 
-Revision ID: 005_add_thumbnail_fields_to_posts
+Revision ID: 005_thumbnail_fields
 Revises: 004_auth_refresh_tokens
 Create Date: 2026-08-27 16:00:00.000000
 
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = '005_add_thumbnail_fields_to_posts'
+revision: str = '005_thumbnail_fields'
 down_revision: Union[str, None] = '004_auth_refresh_tokens'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
